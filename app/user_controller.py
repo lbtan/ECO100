98fe75ccbc1ddfbca8667ff5791c3da31cc7ba55
Hita Gupta <61561837+hitagu@users.noreply.github.com>
2024-03-28 15:31:17 -0400

    Appointment page looks good

--- app/user_controller.py
+++ app/user_controller.py
@@ -32,7 +32,7 @@ def studentview():
     return response
 
 @app.route('/tutorview')
-def tutor():
+def tutorview():
     appointments = db_tutor.get_times_tutors()
     # user id info
     #TODO fetch info from CAS
@@ -42,6 +42,10 @@ def tutor():
     apt_times = utils.appointments_by_time(appointments)
     html_code = flask.render_template('tutorview.html', appointments_by_date=apt_times, user=user, apt_tutor=apt_tutor)
     response = flask.make_response(html_code)
+
+    response.set_cookie('user_name', user[0])
+    response.set_cookie('user_type', user[1])
+    response.set_cookie('user_netid', user[2])
     return response
 
 @app.route('/adminview')
@@ -52,6 +56,10 @@ def adminview():
 
     html_code = flask.render_template('adminview.html', user=user, appointments_by_date=apt_times)
     response = flask.make_response(html_code)
+
+    response.set_cookie('user_name', user[0])
+    response.set_cookie('user_type', user[1])
+    response.set_cookie('user_netid', user[2])
     return response
 
 @app.route('/appointment_page')
@@ -60,11 +68,24 @@ def appointment_page():
     date = flask.request.args.get('date')
     time = flask.request.args.get('time')
 
+    name = flask.request.cookies.get('user_name')
+    type = flask.request.cookies.get('user_type')
+    netid = flask.request.cookies.get('user_netid')
+    user = (name, type, netid)
+
     datetime_str = f"{date} {time}"
     appt_time = datetime.strptime(datetime_str, '%Y-%m-%d %I:%M %p')
     appts = db_queries.get_appointments({"tutor_netid": tutor, "exact_time": appt_time})
+    appt = appts[0] # should only match one appointment
+
+    tutor = db_queries.get_user_info({"netid": appt.get_tutor_netid(), "user_type": "tutor"})[0]
+
+    if appt.get_student_netid():
+        student = db_queries.get_user_info({"netid": appt.get_student_netid(), "user_type": "student"})[0]
+    else:
+        student = None
 
     # https://stackoverflow.com/questions/42601478/flask-calling-python-function-on-button-onclick-event
-    html_code = flask.render_template('appointment_page.html', appointment=appts[0])
+    html_code = flask.render_template('appointment_page.html', appointment=appt, user=user, tutor=tutor, student=student)
     response = flask.make_response(html_code)
     return response
