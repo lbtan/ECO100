ea5557c718ded38e8c90298e3fda0270206d4f68
Hita Gupta <61561837+hitagu@users.noreply.github.com>
2024-04-11 20:35:41 -0400

    updated popups for admin view: tutor overview and weekly summary

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,14 +2,12 @@
 $(document).ready(function(){
 
     function show_appt() {
-        console.log("Button clicked.");
         var params = {
             tutor_netid: $(this).data('tutor_netid'),
             date: $(this).data('date'),
             time: $(this).data('time')
         };
         var urlParams = $.param(params);
-        console.log(params) 
 
         // Load the popup HTML using AJAX
         $.get('/appointment_popup?' + urlParams, function(html) {
@@ -22,7 +20,31 @@ $(document).ready(function(){
         });
     }
 
+    function show_tutor_overview() {
+        // Load the popup HTML using AJAX
+        $.get('/tutor_overview', function(html) {
+            // Create a jQuery object from the HTML string
+            var $modal = $(html);
+            // Add the modified HTML to the modal container and show the modal
+            $('#overview-modal-container').html($modal);
+            $('#overviewModal').modal('show');
+        });
+    }
+
+    function show_weekly_summary() {
+        // Load the popup HTML using AJAX
+        $.get('/weekly_summary', function(html) {
+            // Create a jQuery object from the HTML string
+            var $modal = $(html);
+            // Add the modified HTML to the modal container and show the modal
+            $('#summary-modal-container').html($modal);
+            $('#summaryModal').modal('show');
+        });
+    }
+
     // Event binding for button click
     $('.time-slot').click(show_appt);
-    $('.view-cancel').click(show_appt)
+    $('.view-cancel').click(show_appt);
+    $('.tutor-overview-btn').click(show_tutor_overview);
+    $('.weekly-summary-btn').click(show_weekly_summary);
 });
